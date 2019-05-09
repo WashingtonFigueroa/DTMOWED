c7a3d33479358ebca11d7f7dab27b27fdee51767
Daniel-Vq <daniel.vq.7@gmail.com>
2019-05-09 17:19:53 -0500

    actualizacion about us

--- pagweb/resources/views/about-us.blade.php
+++ pagweb/resources/views/about-us.blade.php
@@ -27,7 +27,7 @@
 				<div class="container">
 					<div class="content-wrapper">
 						<section class="about-us-wrapper">
-							<h2>Sobre Nosotros</h2>
+							<h2 class="section-title">Sobre Nosotros</h2>
 							<div class="row"> 
 									<p align="justify">Desarrolladores Tecnológicos MOWED es una empresa legalmente registrada en la superintendencia de 
 									compañías del Ecuador con una existencia aproximada de 4 años en el mercado que nació con la necesidad de cubrir una 
@@ -76,93 +76,7 @@
 									</ul> 
 							</div>
 							</div>
-							<h2>Metodología ICONIX</h2>
-							<div class="row">
-								<div class="col-md-12">
-									<em>Es una metodología pesada-ligera de Desarrollo del Software que se halla  entre RUP (Rational Unified 
-									Process) y XP (eXtreme Programming), unifica un conjunto de métodos de orientación a objetos con el objetivo 
-									de tener un control estricto sobre todo el ciclo de vida del producto a realizar.</em>
-								</div>
-							</div>
 						</section><!-- /.about-us-wrapper -->
-
-						<section class="about-us-slider-wrapper">
-							<div id="about-us-slider" class="carousel slide" data-ride="carousel">
-							  <!-- Wrapper for slides -->
-							  <div class="carousel-inner" role="listbox">
-							    <div class="item active">
-								    <div class="row">
-								    	<div class="col-md-6">
-								    		<img class="img-responsive" src="estilos/img/abt-slider1.jpg" alt="">
-								    	</div>
-								    	<div class="col-md-6">
-									    	<div class="abt-slider-intro">
-												<br>
-									    		<ul>
-													<li>1. Escuchamos las necesidades del cliente.</li>
-													<li>2. Propuesta del alcance y presupuesto del proyecto.</li>
-													<li>3. Proceso de contratación de acuerdo a los requerimientos de cada cliente.</li>
-													<li>4. Reunión de apertura del proyecto con los responsables de las partes interesadas.</li>
-													<li>5. Levantamiento de información.</li>
-												</ul>
-												<br>
-									    	</div>
-								    	</div>
-								    </div>
-							    </div><!-- /.item -->
-
-							    <div class="item">
-								    <div class="row">
-								    	<div class="col-md-6">
-								    		<img class="img-responsive" src="estilos/img/abt-slider2.jpg" alt="">
-								    	</div>
-								    	<div class="col-md-6">
-									    	<div class="abt-slider-intro">
-									    		<ul>
-													<br>
-													<li> 6. Definición y prototipo de la herramienta.</li>
-													<li> 7. Definición del cronograma ajustado del proyecto con reuniones de seguimiento.</li>
-													<li> 8. Diseño de la base de datos y su diccionario de datos.</li>
-													<li> 9. Implementación del sistema por módulos.</li>
-													<li> 10. Pruebas preliminares.</li>
-												</ul><br>
-									    	</div>
-								    	</div>
-								    </div>
-							    </div><!-- /.item -->
-								  <div class="item">
-									  <div class="row">
-										  <div class="col-md-6">
-											  <img class="img-responsive" src="estilos/img/abt-slider3.jpg" alt="">
-										  </div>
-										  <div class="col-md-6">
-											  <div class="abt-slider-intro">
-												  <ul>
-													  <br>
-													 <li> 11. Implementación de cambios y correcciones.</li>
-													 <li> 12. Pruebas finales.</li>
-													 <li> 13. Manual de instalación, usuario y documentos de soporte.</li>
-													 <li> 14. Instalación de la herramienta de acuerdo a los requisitos del cliente.</li>
-													 <li> 15. Capacitaciones a los usuarios e interesados.</li>
-													 <li> 16. Garantía y servicio post venta.</li>
-												  </ul><br>
-											  </div>
-										  </div>
-									  </div>
-								  </div><!-- /.item -->
-							  </div><!-- /.carousel-inner -->
-
-							  <!-- Controls -->
-							  <a class="left carousel-control" href="#about-us-slider" role="button" data-slide="prev">
-							    <i class="flaticon-arrowhead4"></i>
-							    <span class="sr-only">Previous</span>
-							  </a>
-							  <a class="right carousel-control" href="#about-us-slider" role="button" data-slide="next">
-							    <i class="flaticon-arrow437"></i>
-							    <span class="sr-only">Next</span>
-							  </a>
-							</div>
-						</section>
 
 						<section class="team-wrapper">
 							<h2 class="section-title">Nuestro Equipo</h2>
@@ -282,7 +196,10 @@
 							<h2 class="section-title">Unete a nuestro equipo</h2>
 							<div class="row">
 								<div class="col-sm-8">
-									<p>Trabaja en DTMowed <br> Ingeniero en Sistemas PERFIL REQUERIDO: <br> Género: Indistinto Formación Académica: Titulado En Ingeniería En Sistemas, Computación Y Afines Experiencia 1 año en adelante , manejando varias áreas o funcionalidades del área de TI. </p>
+									<p>Trabaja en DTMowed <br> Ingeniero en Sistemas PERFIL REQUERIDO: 
+									<br> Género: Indistinto Formación Académica: Titulado En Ingeniería En Sistemas, 
+									Computación Y Afines Experiencia 1 año en adelante , manejando varias áreas o 
+									funcionalidades del área de TI. </p>
 								</div>
 								<div class="col-sm-4">
 									
@@ -295,14 +212,101 @@
 									<a class="btn btn-primary joblist-btn" href="contact">Contactanos  <i style="" class="fa fa-angle-right "></i> </a>
 
 								</div>
+							</div> <br>
+							<h2 class="section-title">Nuestra Metodología: ICONIX</h2>
+							<div class="row">
+								<div class="col-md-12">
+									<em>Es una metodología pesada-ligera de Desarrollo del Software que se halla  entre RUP (Rational Unified 
+									Process) y XP (eXtreme Programming), unifica un conjunto de métodos de orientación a objetos con el objetivo 
+									de tener un control estricto sobre todo el ciclo de vida del producto a realizar.</em>
+								</div>
+							</div>
+						</section>
+
+						<section class="about-us-slider-wrapper">
+						
+							<div id="about-us-slider" class="carousel slide" data-ride="carousel">
+							  <!-- Wrapper for slides -->
+							  <div class="carousel-inner" role="listbox">
+							    <div class="item active">
+								    <div class="row">
+								    	<div class="col-md-6">
+								    		<img class="img-responsive" src="estilos/img/abt-slider1.jpg" alt="">
+								    	</div>
+								    	<div class="col-md-6">
+									    	<div class="abt-slider-intro">
+												<br>
+									    		<ul>
+													<li>1. Escuchamos las necesidades del cliente.</li>
+													<li>2. Propuesta del alcance y presupuesto del proyecto.</li>
+													<li>3. Proceso de contratación de acuerdo a los requerimientos de cada cliente.</li>
+													<li>4. Reunión de apertura del proyecto con los responsables de las partes interesadas.</li>
+													<li>5. Levantamiento de información.</li>
+												</ul>
+												<br>
+									    	</div>
+								    	</div>
+								    </div>
+							    </div><!-- /.item -->
+
+							    <div class="item">
+								    <div class="row">
+								    	<div class="col-md-6">
+								    		<img class="img-responsive" src="estilos/img/abt-slider2.jpg" alt="">
+								    	</div>
+								    	<div class="col-md-6">
+									    	<div class="abt-slider-intro">
+									    		<ul>
+													<br>
+													<li> 6. Definición y prototipo de la herramienta.</li>
+													<li> 7. Definición del cronograma ajustado del proyecto con reuniones de seguimiento.</li>
+													<li> 8. Diseño de la base de datos y su diccionario de datos.</li>
+													<li> 9. Implementación del sistema por módulos.</li>
+													<li> 10. Pruebas preliminares.</li>
+												</ul><br>
+									    	</div>
+								    	</div>
+								    </div>
+							    </div><!-- /.item -->
+								  <div class="item">
+									  <div class="row">
+										  <div class="col-md-6">
+											  <img class="img-responsive" src="estilos/img/abt-slider3.jpg" alt="">
+										  </div>
+										  <div class="col-md-6">
+											  <div class="abt-slider-intro">
+												  <ul>
+													  <br>
+													 <li> 11. Implementación de cambios y correcciones.</li>
+													 <li> 12. Pruebas finales.</li>
+													 <li> 13. Manual de instalación, usuario y documentos de soporte.</li>
+													 <li> 14. Instalación de la herramienta de acuerdo a los requisitos del cliente.</li>
+													 <li> 15. Capacitaciones a los usuarios e interesados.</li>
+													 <li> 16. Garantía y servicio post venta.</li>
+												  </ul><br>
+											  </div>
+										  </div>
+									  </div>
+								  </div><!-- /.item -->
+							  </div><!-- /.carousel-inner -->
+
+							  <!-- Controls -->
+							  <a class="left carousel-control" href="#about-us-slider" role="button" data-slide="prev">
+							    <i class="flaticon-arrowhead4"></i>
+							    <span class="sr-only">Previous</span>
+							  </a>
+							  <a class="right carousel-control" href="#about-us-slider" role="button" data-slide="next">
+							    <i class="flaticon-arrow437"></i>
+							    <span class="sr-only">Next</span>
+							  </a>
 							</div>
 						</section>
 
 						<section class="download-box-wrapper">
 							<div class="download-box">
-								<ul class="list-inline">
+								<ul align= "center" class="list-inline">
 									<li><a target="_blank" href="ds">DESARROLLO DE SOFTWARE <span> Visitar </span></a></li>
-									<li><a target="_blank" href="mercadotecnia">MERCADOTECNIA <span> Visitar </span></a></li>
+									<!--<li><a target="_blank" href="mercadotecnia">MERCADOTECNIA <span> Visitar </span></a></li>-->
 									<li><a target="_blank" href="capacitacion">CAPACITACIONES <span> Visitar </span></a></li>
 								</ul>
 							</div>
--- pagweb/resources/views/ds.blade.php
+++ pagweb/resources/views/ds.blade.php
@@ -28,13 +28,6 @@
                     <div class="content-wrapper">
                         <div class="inner-content">
                         <h3>Diseño y Desarrollo de Software a la medida</h3>
-                            <!-- <ul class="list-inline" id="filter">
-                                <li><a class="active" data-group="all">Todos</a></li>
-                                <li><a data-group="red">Software</a></li>
-                                <li><a data-group="green">Aplicaciones Móviles</a></li>
-                                <li><a data-group="blue">Mercadeo</a></li>
-                                <li><a data-group="numbers">Diseño y Publicidad</a></li>
-                            </ul> -->
                             <div class="row">
                                 <div id="grid">
                                 <h4>Sistema de Facturación</h4>
@@ -48,7 +41,7 @@
                                         </div><!-- /.single-portfolio -->
                                     </div>
 
-                                    <div class="col-md-8">
+                                    <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Nuestro Sistema de Facturación ofrece las prestaciones esenciales para que empieces tu
                                             negocio, especialmente para supermercados, expensas, minimarkets, ferreterías, papelerías y otros
                                             tipos de comercios sin realizar una gran inversión o para implementarlo y continuar creciendo tu 
@@ -61,7 +54,7 @@
                             <div class="row">
                                 <div id="grid">
                                 <h4 align="right">Sistema de Facturación Electrónica</h4>
-                                <div class="col-md-8">
+                                <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">La facturación electrónica se ha vuelto un avance tecnológico muy necesario para el crecimiento
                                             de cualquier negocio, por lo cual, ofrecemos la solución más completa y sencilla de 
                                             emitir, autorizar y distribuir tus comprobantes electrónicos.</p>
@@ -93,7 +86,7 @@
                                         </div><!-- /.single-portfolio -->
                                     </div>
 
-                                    <div class="col-md-8">
+                                    <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Es un sistema que te ahorrará mucho tiempo y esfuerzo en la gestión de tus documentos digitales, 
                                             de una manera segura y confiable.</p>
                                         <p><a class="btn btn-primary wow fadeInDown" href="http://demo.coatlfact.com/#/" role="button">Ver Demo</a></p>
@@ -104,7 +97,7 @@
                             <div class="row">
                                 <div id="grid">
                                 <h4 align="right">Administración Junta de Aguas</h4>
-                                <div class="col-md-8">
+                                <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Este sistema permite una gestión integral para las Comunidades, permitiendo así facilitar el
                                              desarrollo y servicio ofrecido a los contribuyentes buscando una mejora y organización en el area administrativa.</p>
                                         <p align="right"><a class="btn btn-primary wow fadeInDown" href="http://demo.coatlfact.com/#/" role="button">Ver Demo</a></p>
@@ -135,7 +128,7 @@
                                         </div><!-- /.single-portfolio -->
                                     </div>
 
-                                    <div class="col-md-8">
+                                    <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Ponemos a disposición un Sistema de tienda en linea adaptable a tu negocio. Este 
                                             sistema permita la publicación y venta de productos con un manejo de inventario seguro y confiable.
                                         </p>
@@ -147,7 +140,7 @@
                             <div class="row">
                                 <div id="grid">
                                 <h4 align="right">Sistema de Envío de Tomografías y Radiografías</h4>
-                                <div class="col-md-8">
+                                <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Es un sistema realizado para clínicas que requieran un soporte digital de los diferentes 
                                             tipos de exámenes que se realizan y de esta manera gestionar adecuadamente su administración.
                                         </p>
@@ -179,7 +172,7 @@
                                         </div><!-- /.single-portfolio -->
                                     </div>
 
-                                    <div class="col-md-8">
+                                    <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Es un sistema de administración y programación de diferentes tipos de entrenamientos 
                                             personalizado para cada cliente.</p>
                                         <p><a class="btn btn-primary wow fadeInDown" href="http://demo.coatlfact.com/#/" role="button">Ver Demo</a></p>
@@ -190,7 +183,7 @@
                             <div class="row">
                                 <div id="grid">
                                 <h4 align="right">Sistema Biométrico y Control de Acceso</h4>
-                                <div class="col-md-8">
+                                <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Este sistema permitirá el monitoreo en tiempo real del acceso a un area o una zona
                                             determinada mediante algún tipo de dato biométrico.
                                             </p>
@@ -222,7 +215,7 @@
                                         </div><!-- /.single-portfolio -->
                                     </div>
 
-                                    <div class="col-md-8">
+                                    <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Es un sistema que nos permite crear y administrar todo tipo de eventos y guardar 
                                             la información necesario para la generación de reportes. Además permite una gestión adecuada de 
                                             los asistentes, así como el registro online para cada evento.</p>
@@ -234,7 +227,7 @@
                             <div class="row">
                                 <div id="grid">
                                 <h4 align="right">Sistema Agroecológico</h4>
-                                <div class="col-md-8">
+                                <div class="col-md-8 wow fadeInDown">
 								        <p align="justify">Este sistema permite la gestión de todas las actividades agroecológicas.</p>
                                         <p align="right"><a class="btn btn-primary wow fadeInDown" href="http://demo.coatlfact.com/#/" role="button">Ver Demo</a></p>
 							        </div>
